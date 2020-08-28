7483c4f0ae71e2389baa24126acbd59e4e8236e2
Reinaldo Ramos <masterrey@gmail.com>
2020-08-28 11:28:25 -0300

    personagem com movimentos melhorados,
    colocado movimentod e cintura, blend tree e novas animaçoes

--- Assets/Scripts/ControlPlayer.cs
+++ Assets/Scripts/ControlPlayer.cs
@@ -2,29 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ControlPlayer : MonoBehaviour
 {
     Vector3 movplayer;
     Rigidbody rdb;
     public GameObject aim;
+    public GameObject aimspine;
     public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody[] rdbs = GetComponentsInChildren<Rigidbody>();
         Joint[] joints = GetComponentsInChildren<Joint>();
-        foreach(Joint joint in joints)
+        foreach (Joint joint in joints)
         {
             Destroy(joint);
         }
-        for(int i=1;i<rdbs.Length; i++)
+        for (int i = 1; i < rdbs.Length; i++)
         {
             Destroy(rdbs[i]);
         }
         rdb = rdbs[0];
         aim.transform.parent = Camera.main.transform;
-    }
 
+        aim.transform.localPosition = Vector3.zero;
+        aim.transform.localRotation = Quaternion.identity;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -35,10 +39,24 @@ public class ControlPlayer : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Vector3 locVelocity = transform.InverseTransformDirection(rdb.velocity); //transforma de mundo pra local
+        Vector3 globMov = transform.TransformDirection(movplayer); //tranforma de local pra mundo
 
+        globMov = new Vector3(globMov.x*3, rdb.velocity.y, globMov.z*3); //remove o y do movimento
 
-        anim.SetFloat("Walk", rdb.velocity.magnitude);
-        rdb.AddRelativeForce(movplayer * 1000);
-        transform.rotation = Quaternion.Lerp(transform.rotation, aim.transform.rotation,Time.fixedDeltaTime);
+        anim.SetFloat("Walk", locVelocity.z); //aplica o valor na animaçao
+        anim.SetFloat("SideWalk", locVelocity.x); 
+       
+        rdb.velocity = globMov ; //modifica a velocidade da fisica
+        float ang = Vector3.Dot(transform.forward, -aim.transform.right);
+        transform.Rotate(transform.up,ang*5);
+        aimspine.transform.rotation = aim.transform.rotation; //aplica a rotacao
     }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+       
+        anim.SetBoneLocalRotation(HumanBodyBones.Spine, aimspine.transform.localRotation);
+    }
+
 }
